b82c204a1f26e05fc0262a5b2e9e056d76adb73b
irldoodle <egaming@mail.mccneb.edu>
2026-01-13 19:37:50 -0600

    Update PlayerController.cs

--- Scripts/PlayerController.cs
+++ Scripts/PlayerController.cs
@@ -3,20 +3,23 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    private InputAction moveAction;
+    private InputAction jumpAction;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        moveAction = InputSystem.actions.FindAction("Move");
+        jumpAction = InputSystem.actions.FindAction("Jump");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if ( jumpAction.WasPressedThisFrame() )
+        {
+
+        }
+        var moveValue = moveAction.ReadValue<Vector2>();
     }
 
-    void OnMove()
-    {
-
-    }
 }
