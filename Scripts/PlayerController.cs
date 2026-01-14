6d9a08e3b629d506ff627f375be12c2c53a3c882
irldoodle <egaming@mail.mccneb.edu>
2026-01-14 15:05:45 -0600

    Further development on PlayerController

--- Scripts/PlayerController.cs
+++ Scripts/PlayerController.cs
@@ -1,15 +1,28 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.LowLevelPhysics2D;
 
 public class PlayerController : MonoBehaviour
 {
     private InputAction moveAction;
     private InputAction jumpAction;
+    private Animator Animator;
+    private SpriteRenderer Sprite;
+    private Rigidbody2D rb;
+
+    public float moveSpeed = 1;
+    public float maxSpeed = 5;
+    public float jumpForce = 3;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         moveAction = InputSystem.actions.FindAction("Move");
         jumpAction = InputSystem.actions.FindAction("Jump");
+        Animator = GetComponent<Animator>();
+
+        //Bind Animator isJumping and isRunning to movement;
+
+
     }
 
     // Update is called once per frame
@@ -17,9 +30,18 @@ public class PlayerController : MonoBehaviour
     {
         if ( jumpAction.WasPressedThisFrame() )
         {
-
+            Animator.SetBool("IsJumping", true);
         }
+
         var moveValue = moveAction.ReadValue<Vector2>();
+
+        if(moveValue.x < 0)
+        {
+            if(rb.totalForce.x + (moveValue.x * moveSpeed) < maxSpeed)
+                rb.AddForceX(moveValue.x * moveSpeed);
+            else if(rb.totalForce.x > maxSpeed)
+                rb.AddForceX(maxSpeed - rb.totalForce.x);
+        }
     }
 
 }
